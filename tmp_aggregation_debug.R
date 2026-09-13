501f89ae6b7d7fddac62c686ec6fd8de99ecbe79
Merkle <muk@ssb.no>
2026-09-13 18:03:22 +0200

    continued inca investigation on cooling rasters

--- tmp_aggregation_debug.R
+++ tmp_aggregation_debug.R
@@ -1,5 +1,181 @@
 
 
+
+################################################################################
+##### try recreate INCA bug ####################################################
+################################################################################
+
+# right, so copilot has identified that there might be a massive bug in the INCA script.
+# the creation of the cooling rasters might be based on wrong inputs, because the code
+# draws from column indeces rather than column names. The column indices indicate that
+# the coefficients imposed to generate the cooling rasters are completely wrong. 
+# it uses mean tree cover as alpha1h, cooling_mean as beta1h and cooling median as gamma1h.
+# then it uses alpha1h as alpha 2h, beta1h as beta2h, and gamma1h as gamma2h.
+# see python script line 491 ff.
+
+# We start at 6.2, the creation of cooling rasters
+
+gc()
+### We run this as a loop across months and cities. Following INCA's python implementation as close as possible
+cooling_list_m <- vector("list", length(mdr))
+
+for(j in 1:length(mdr)){
+# make a list for the city-wise cooling rasters
+cooling_list <- vector("list", nrow(cityb_buf))
+for(i in 1:nrow(cityb_buf)){
+  # get coefficients (wrongly)
+  alpha1h_ <- firstdf$avg_tcd[firstdf$month==mdr[j] & firstdf$city==cityb_buf$LAU_NAME[i]]
+  beta1h_ <- cooldf$average_cooling[cooldf$month==mdr[j] & cooldf$city==cityb_buf$LAU_NAME[i]]
+  gamma1h_ <- cooldf$median_cooling[cooldf$month==mdr[j] & cooldf$city==cityb_buf$LAU_NAME[i]]
+  # create simulated lst rasters
+  lst_sim_green_ <- alpha1h_ + beta1h_*crop(tcdr_no,cityb_buf[i,],mask=T) + gamma1h_*crop(evapr_no,cityb_buf[i,],mask=T)
+  lst_sim_gray_ <- alpha1h_ + 0*crop(tcdr_no,cityb_buf[i,],mask=T) + 0*crop(evapr_no,cityb_buf[i,],mask=T)
+  # create wrong second regression coefficients
+  alpha2h_ <- firstdf$est_intercept[firstdf$month==mdr[j] & firstdf$city==cityb_buf$LAU_NAME[i]]
+  beta2h_ <- firstdf$est_tcd[firstdf$month==mdr[j] & firstdf$city==cityb_buf$LAU_NAME[i]]
+  gamma2h_ <- firstdf$est_evap[firstdf$month==mdr[j] & firstdf$city==cityb_buf$LAU_NAME[i]]
+  # create simulated air temp rasters wrongly
+  t_air_green_ <- alpha2h_ + beta2h_*lst_sim_green_ + gamma2h_*crop(lat_r,cityb_buf[i,],mask=T)
+  t_air_gray_ <- alpha2h_ + beta2h_*lst_sim_gray_ + gamma2h_*crop(lat_r,cityb_buf[i,],mask=T)
+  # cooling
+  cooling_ <- t_air_gray_ - t_air_green_
+  # as in INCA we force negative values to be zero
+  cooling_[cooling_<0] <- 0
+  # save in stack
+  cooling_list[[i]]<-cooling_
+  # tidy up
+  rm(alpha1h_,beta1h_,gamma1h_,lst_sim_gray_,lst_sim_green_,t_air_gray_,t_air_green_,cooling_,alpha2h_,beta2h_,gamma2h_)
+}
+# in this function now we put the cities together and impose the mean cooling for the overlapping boundaries
+cooling_list_m[[j]] <- do.call(terra::mosaic,c(cooling_list,fun=mean))
+# save
+writeRaster(cooling_list_m[[j]],file.path(rfp,paste0("cooling_wrong_",j,".tif")),overwrite=T)
+}
+cooling_stack_wrong<-terra::rast(cooling_list_m)
+names(cooling_stack_wrong)<-mdr
+rm(cooling_list_m)
+
+
+# plot for july
+j=2
+for(i in 1:nrow(cityb_buf)){
+  plot(crop(cooling_stack[[j]], cityb_buf[i,],mask=T),main=paste(cityb_buf$LAU_NAME[i],"cooling","month",mdr[j]), legend=T)
+  plot(roads_list[[i]],add=T)
+}
+
+
+
+# this is INCA cooling for OSLO
+plot(crop(inca_cooling,cityb_buf[1,],mask=T))
+# here is ours
+plot(crop(cooling_stack[[2]], cityb_buf[1,],mask=T), legend=T)
+# damn we clearly have found the problem here.... Crazy.
+##########################################################
+### now make a table with the wrong data
+gc()
+# now cooling by nuts and ecosystem
+# Import Norway NUTs2 shape
+n2shp<-terra::vect(file.path(d_fp,"Norway_files","NUTS2021_NO_LVL2_wosvalbard","NUTS2021_NO_LVL2_wos.shp"))
+n2shp <- project(n2shp,crs(lcm))
+# rasterize the shape
+n2r_no <- terra::rasterize(n2shp,mask_stack,field="NUTS_ID")
+# then we need lcm, make sure it is aligned with the rest of the raster data
+lcm2 <- resample(lcm,mask_stack,method='near')
+# now make a loop across months
+stl_wrong<-list()
+for(j in 1:length(mdr)){
+  # Step 1: Make a stack of the relevant data and move it into a dataframe
+  astack <- c(
+    cooling_stack_wrong[[j]],
+    n2r_no,
+    lcm2)
+  names(astack)<-c("cooling","region","lcm")
+  adf<-as.data.frame(astack)
+  clean_adf <- adf %>% drop_na()
+  # Step 2: Retrieve all the different summary statistics we want
+  # means by region and ecosystem type
+  mean_by_region_es <- clean_adf %>% 
+    group_by(region,lcm) %>% 
+    summarise(cooling = mean(cooling))
+  mean_by_es <- clean_adf %>% 
+    select(-region) %>% 
+    group_by(lcm) %>% 
+    summarise(cooling = mean(cooling)) %>%
+    mutate(region = "NO") %>%
+    relocate(region,.before=lcm)
+  # total means by NUTS2
+  mean_by_region <- clean_adf %>% 
+    group_by(region) %>% 
+    summarise(cooling = mean(cooling)) %>% 
+    mutate(lcm="All") %>%
+    relocate(lcm,.after=region)
+  # total mean for the whole country NUTS0
+  mean_overall <- clean_adf %>% select(cooling) %>% 
+    summarise(cooling=mean(cooling)) %>%
+    mutate(region = "NO",lcm = "All") %>%
+    relocate(cooling,.after =lcm)
+  # put together
+  mean_vals <- rbind(mean_by_es,mean_overall,mean_by_region_es,mean_by_region) %>% 
+    filter(region !="NO09") %>% 
+    pivot_wider(names_from=lcm,values_from = cooling) %>%
+    left_join(n2shp %>% as.data.frame() %>% select(NUTS_ID,NUTS_NAME),
+              by = join_by(region == NUTS_ID)) %>%
+    rename(Name = NUTS_NAME) %>%
+    relocate(Name,.after=region) %>%
+    rename(Region = region)
+  mean_vals$Name[mean_vals$Region=="NO"]<-"Norge"
+  # put into our list
+  stl_wrong[[j]]<-mean_vals
+  # Step 3: clean up
+  rm(astack,adf,clean_adf,mean_overall,mean_by_region,mean_by_es,mean_by_region_es,mean_vals)
+}
+names(stl_wrong)<-mdr
+
+
+test<-stl_wrong[[j]]
+### ok so I still do not get the same SUT as INCA, but the cooling raster is now
+# more similar. I suspect the next bug with the aggregation across lcm categories
+
+# check totals
+sum(values(cooling_stack_wrong[[j]]),na.rm=T)
+sum(values(inca_cooling,na.rm=T))
+# ok INCA sum is still three times larger than our sum. Look at this by city
+j=2
+
+i=1
+
+plot(mask())
+
+
+plot(crop(cooling_stack_wrong[[j]],cityb_buf[i,],mask=T))
+plot(crop(inca_cooling,cityb_buf[i,],mask=T))
+
+ext(inca_cooling)
+ext(cooling_stack_wrong)
+inca_cooling <- terra::resample(inca_cooling,cooling_stack_wrong)
+
+i=8
+plot(crop(inca_cooling,cityb[i,],mask=T) - crop(cooling_stack_wrong[[j]],cityb[i,],mask=T))
+
+## main observation: The cooling rasters are pretty close to each other. 
+# So I wonder where the value sum difference comes from. Let's have a look at the overall map
+
+plot(inca_cooling-cooling_stack_wrong[[j]])
+# change colour scheme so we can see differences better
+sum(values(inca_cooling),na.rm=T)
+sum(values(cooling_stack_wrong[[j]]),na.rm=T)
+# no actually the sum is quite close now. So the large sum before resampling
+# needs to be because some other regions get included?
+
+
+inca_cooling<-rast(file.path(here::here(),"tmp_inca_cooling.tif"))
+plot(inca_cooling)
+# right so here we need to dig more. Where do the value differences come from 
+# between the resampled cooling map and the original one.
+
+
+################################################################################
+
 # now cooling by nuts and ecosystem
 
 gc()
@@ -194,270 +370,3 @@ summary(values(cooling_stack[[j]]))
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#################################################################################
-
-alpha1hr_7 <- terra::rasterize(cityb_buf_7,mask_stack[[j]],field="est_intercept",fun=mean)
-beta1hr_7 <- terra::rasterize(cityb_buf_7,mask_stack[[j]],field="est_tcd",fun=mean)
-gamma1hr_7 <- terra::rasterize(cityb_buf_7,mask_stack[[j]],field="est_evap",fun=mean)
-
-lst_sim_green_7 <- alpha1hr_7 + beta1hr_7*tcdr_no + gamma1hr_7*evapr_no
-lst_sim_gray_7 <- alpha1hr_7 
-
-t_air_green_7 <- alpha2h + beta2h*lst_sim_green_7 + gamma2h*lat_r
-t_air_gray_7 <- alpha2h + beta2h*lst_sim_gray_7 + gamma2h*lat_r
-
-cooling_7 <- t_air_gray_7 - t_air_green_7
-plot(cooling_7)
-cooling_nn_7 <- cooling_7
-cooling_nn_7[cooling_nn_7<0]<-0
-
-plot(cooling_nn_7)
-
-plot(crop(cooling_nn_7,cityb[8,],mask=T))
-
-################################################################################
-j=2
-
-#first we create a latitude raster
-# Project raster to geographic coordinates
-tmp_ll <- project(lstr_stack[[1]], "EPSG:4326")
-# Create latitude raster
-lat_ll <- init(tmp_ll, "y")
-# Back to the original grid
-lat_r <- project(lat_ll, lstr_stack[[1]])
-rm(tmp_ll,lat_ll)
-
-# estimate lst with vegetation
-lstvr <- intercept_stack_m[[j]] + beta1_stack_m[[j]]*tcdr_no + gamma1_stack_m[[j]]*evapr_no
-plot(lstvr)
-
-# then air temperature with vegetation
-tairmv <- alpha2h + beta2h*lstvr + gamma2h*lat_r
-# and without vegetation
-tairuv <- alpha2h + beta2h*intercept_stack_m[[j]] + gamma2h*lat_r
-plot(tairmv)
-plot(tairuv)
-
-i=7
-#air temp
-plot(crop(tairmv,cityb_buf[i,],mask=T))
-plot(crop(tairuv,cityb_buf[i,],mask=T))
-
-# look at inputs to tairuv
-plot(crop(intercept_stack_m[[j]],cityb_buf[i,],mask=T))
-plot(crop(lat_r,cityb_buf[i,],mask=T))
-
-
-mean(values(crop(tairmv,cityb_buf[i,],mask=T),na.rm=T))
-mean(values(crop(tairuv,cityb_buf[i,],mask=T),na.rm=T))
-
-mean(values(crop(tairuv,cityb_buf[i,],mask=T),na.rm=T)) - mean(values(crop(tairmv,cityb_buf[i,],mask=T),na.rm=T))
-
-#lst
-plot(crop(lstvr,cityb_buf[i,],mask=T))
-mean(values(crop(lstvr,cityb_buf[i,],mask=T),na.rm=T))
-
-
-
-
-
-
-
-
-
-plot(cooling_stack[[2]])
-
-
-i=1
-plot(crop(cooling_stack[[2]],cityb_buf[i,],mask=T))
-mean(values(crop(cooling_stack[[2]],cityb_buf[i,],mask=T),na.rm=T))
-median(values(crop(cooling_stack[[2]],cityb_buf[i,],mask=T),na.rm=T))
-
-plot(crop(cooling_stack_um[[2]],cityb_buf[i,],mask=T))
-mean(values(crop(cooling_stack_um[[2]],cityb_buf[i,],mask=T),na.rm=T))
-median(values(crop(cooling_stack_um[[2]],cityb_buf[i,],mask=T),na.rm=T))
-
-plot(crop(tcdr_no,cityb_buf[i,],mask=T))
-plot(crop(evapr_no,cityb_buf[i,],mask=T))
-
-
-
-
-j=2 # means july
-
-
-df_sut<-as.data.frame(matrix(ncol=14,nrow=nrow(cityb_buf)))
-colnames(df_sut)<-c("LAU",estv,"w_avg_cool")
-
-
-i=8 # means first city
-
-
-# the LAU name
-
-r <- mask(
-  crop(cooling_stack[[j]], cityb_buf[i, ]),
-  cityb_buf[i, ]
-)
-plot(r)
-
-
-lcm_city <- crop(lcm2, r)
-r <- mask(r, lcm_city)
-plot(r)
-
-global(r, median, na.rm = TRUE)
-global(r, mean, na.rm = TRUE)
-
-plot(crop(lstr_stack[[i]],cityb_buf[i,]))
-
-# we are not exactly alike INCA, but close. I wonder whether this is related to 
-# that they have masked out some of the missing inputs, but not all?
-
-# test whether it makes a difference to apply the non-simplified calculation.
-# done. it does not make a difference.
-
-
-
-# check that averages are based on the masked data
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-df_sut$LAU[i]<-as.data.frame(cityb_buf[i,])$LAU_NAME
-# compute cooling averages
-# using terra zonal
-z <- terra::zonal(mask(crop(cooling_stack[[j]], cityb_buf[i,]),cityb_buf[i,]), # values
-                  mask(crop(lcm2, cityb_buf[i,]),cityb_buf[i,]),# categories
-                  fun="mean",
-                  na.rm=T)
-
-r <- mask(
-  crop(cooling_stack[[j]], cityb_buf[i, ]),
-  cityb_buf[i, ]
-)
-lcm_city <- crop(lcm2, r)
-
-r <- mask(r, lcm_city)
-
-global(r, median, na.rm = TRUE)
-global(r, mean, na.rm = TRUE)
-plot(r)
-
-# right so here I get a value of -2.109 as a mean, 
-# while I got -2.16 as a weighted mean in the table. (which is what we see as INCA result, too)
-# how is that possible? Need to do more digging, to find out how our code differs from INCA here.
-
-## adjusted version
-cool <- mask(crop(cooling_stack[[j]], cityb_buf[i,]), cityb_buf[i,])
-lcm_city <- mask(crop(lcm2, cityb_buf[i,]), cityb_buf[i,])
-cool_lcm <- mask(lcm_city, cool)
-# set column names
-colnames(z) <- c("es_id","cooling")
-# add ecosystem names
-z$es_name <- lcm_idf$es_type[
-  match(z$es_id, lcm_idf$es_id)]
-# here is the weighted cooling across all ecosystems
-dfp<-freq(cool_lcm)
-dfp['es_weight']<-dfp$count / dfp %>% select(count) %>% sum()
-z <- z %>% left_join(dfp,by=join_by("es_id" == "value"))
-z['weighted_average_cooling']<-z$cooling*z$es_weight
-sum(z$weighted_average_cooling)
-# Ok, what I notice here is that the weighted average that we get out here IS NOT the same as what we get out from our direct raster operations using the global function...
-
-
-cool <- mask(crop(cooling_stack[[j]], cityb_buf[i,]), cityb_buf[i,])
-lcm_city <- mask(crop(lcm2, cityb_buf[i,]), cityb_buf[i,])
-
-table(
-  is.na(values(cool)),
-  is.na(values(lcm_city))
-)
-
-
-
-
-rm(dfp)
-# paste result into our table, loop again, just to ensure we keep empty values where ecosystems do not exist
-for(k in 1:length(estv)){
-  df_sut[i,k+1]<- {
-    x <- z %>% filter(es_name==estv[k]) %>% pull(cooling)
-    if(length(x) == 0) NA else x
-  }
-  rm(x)
-}
-# finally the weighted average for the LAU as a whole
-df_sut$w_avg_cool[i]<-sum(z$weighted_average_cooling)
-rm(z)
-
-
-
-# now loop across LAU
-for(i in 1:nrow(cityb_buf)){
-  # the LAU name
-  df_sut$LAU[i]<-as.data.frame(cityb_buf[i,])$LAU_NAME
-  # compute cooling averages
-  # using terra zonal
-  z <- terra::zonal(mask(crop(cooling_stack[[j]], cityb_buf[i,]),cityb_buf[i,]), # values
-                    mask(crop(lcm2, cityb_buf[i,]),cityb_buf[i,]),# categories
-                    fun="mean",
-                    na.rm=T)
-  # set column names
-  colnames(z) <- c("es_id","cooling")
-  # add ecosystem names
-  z$es_name <- lcm_idf$es_type[
-    match(z$es_id, lcm_idf$es_id)]
-  # here is the weighted cooling across all ecosystems
-  dfp<-freq(mask(crop(lcm2, cityb_buf[i,]),cityb_buf[i,]))
-  dfp['es_weight']<-dfp$count / freq(mask(crop(lcm2, cityb_buf[i,]),cityb_buf[i,])) %>% select(count) %>% sum()
-  z <- z %>% left_join(dfp,by=join_by("es_id" == "value"))
-  z['weighted_average_cooling']<-z$cooling*z$es_weight
-  rm(dfp)
-  # paste result into our table, loop again, just to ensure we keep empty values where ecosystems do not exist
-  for(k in 1:length(estv)){
-    df_sut[i,k+1]<- {
-      x <- z %>% filter(es_name==estv[k]) %>% pull(cooling)
-      if(length(x) == 0) NA else x
-    }
-    rm(x)
-  }
-  # finally the weighted average for the LAU as a whole
-  df_sut$w_avg_cool[i]<-sum(z$weighted_average_cooling)
-  rm(z)
-}
